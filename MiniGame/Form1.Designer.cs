5d4f546dab89301463fc319504a9b9dd443aeb00
Csaba07 <aftcsaba@gmail.com>
2025-04-07 15:04:31 +0200

    04.07.

--- MiniGame/Form1.Designer.cs
+++ MiniGame/Form1.Designer.cs
@@ -28,25 +28,31 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.statusBar = new System.Windows.Forms.ToolStripStatusLabel();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.témaVálasztóToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.játákosokToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.themeSelectorTSM = new System.Windows.Forms.ToolStripMenuItem();
+            this.kékToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sárgaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zöldToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.playersMenuStrip = new System.Windows.Forms.ToolStripMenuItem();
             this.névjegyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.playersPanel = new System.Windows.Forms.Panel();
             this.SaveBtn = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.kékToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.sárgaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.zöldToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.player2tB = new System.Windows.Forms.TextBox();
+            this.player1tB = new System.Windows.Forms.TextBox();
+            this.movementscB = new System.Windows.Forms.ComboBox();
+            this.Player2Pb = new System.Windows.Forms.PictureBox();
+            this.Player1Pb = new System.Windows.Forms.PictureBox();
             this.mapPb = new System.Windows.Forms.PictureBox();
             this.statusStrip1.SuspendLayout();
             this.menuStrip1.SuspendLayout();
             this.playersPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Player2Pb)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Player1Pb)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.mapPb)).BeginInit();
             this.SuspendLayout();
             // 
@@ -54,9 +60,9 @@
             // 
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.statusBar});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 567);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.Size = new System.Drawing.Size(910, 22);
             this.statusStrip1.TabIndex = 0;
             this.statusStrip1.Text = "statusStrip1";
             // 
@@ -68,46 +74,69 @@
             // menuStrip1
             // 
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.témaVálasztóToolStripMenuItem,
-            this.játákosokToolStripMenuItem,
+            this.themeSelectorTSM,
+            this.playersMenuStrip,
             this.névjegyToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(910, 24);
             this.menuStrip1.TabIndex = 1;
             this.menuStrip1.Text = "menuStrip1";
             // 
-            // témaVálasztóToolStripMenuItem
+            // themeSelectorTSM
             // 
-            this.témaVálasztóToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.themeSelectorTSM.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.kékToolStripMenuItem,
             this.sárgaToolStripMenuItem,
             this.zöldToolStripMenuItem});
-            this.témaVálasztóToolStripMenuItem.Name = "témaVálasztóToolStripMenuItem";
-            this.témaVálasztóToolStripMenuItem.Size = new System.Drawing.Size(92, 20);
-            this.témaVálasztóToolStripMenuItem.Text = "Téma választó";
+            this.themeSelectorTSM.Name = "themeSelectorTSM";
+            this.themeSelectorTSM.Size = new System.Drawing.Size(92, 20);
+            this.themeSelectorTSM.Text = "Téma választó";
             // 
-            // játákosokToolStripMenuItem
+            // kékToolStripMenuItem
             // 
-            this.játákosokToolStripMenuItem.Name = "játákosokToolStripMenuItem";
-            this.játákosokToolStripMenuItem.Size = new System.Drawing.Size(127, 20);
-            this.játákosokToolStripMenuItem.Text = "Játákosok megadása";
-            this.játákosokToolStripMenuItem.Click += new System.EventHandler(this.GetPlayers);
+            this.kékToolStripMenuItem.Name = "kékToolStripMenuItem";
+            this.kékToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.kékToolStripMenuItem.Text = "Kék";
+            this.kékToolStripMenuItem.Click += new System.EventHandler(this.ThemeClick);
+            // 
+            // sárgaToolStripMenuItem
+            // 
+            this.sárgaToolStripMenuItem.Name = "sárgaToolStripMenuItem";
+            this.sárgaToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.sárgaToolStripMenuItem.Text = "Sárga";
+            this.sárgaToolStripMenuItem.Click += new System.EventHandler(this.ThemeClick);
+            // 
+            // zöldToolStripMenuItem
+            // 
+            this.zöldToolStripMenuItem.Name = "zöldToolStripMenuItem";
+            this.zöldToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.zöldToolStripMenuItem.Text = "Zöld";
+            this.zöldToolStripMenuItem.Click += new System.EventHandler(this.ThemeClick);
+            // 
+            // playersMenuStrip
+            // 
+            this.playersMenuStrip.Enabled = false;
+            this.playersMenuStrip.Name = "playersMenuStrip";
+            this.playersMenuStrip.Size = new System.Drawing.Size(127, 20);
+            this.playersMenuStrip.Text = "Játékosok megadása";
+            this.playersMenuStrip.Click += new System.EventHandler(this.GetPlayers);
             // 
             // névjegyToolStripMenuItem
             // 
             this.névjegyToolStripMenuItem.Name = "névjegyToolStripMenuItem";
             this.névjegyToolStripMenuItem.Size = new System.Drawing.Size(62, 20);
             this.névjegyToolStripMenuItem.Text = "Névjegy";
+            this.névjegyToolStripMenuItem.Click += new System.EventHandler(this.ShowMakers);
             // 
             // playersPanel
             // 
             this.playersPanel.Controls.Add(this.SaveBtn);
             this.playersPanel.Controls.Add(this.label2);
             this.playersPanel.Controls.Add(this.label1);
-            this.playersPanel.Controls.Add(this.textBox2);
-            this.playersPanel.Controls.Add(this.textBox1);
-            this.playersPanel.Location = new System.Drawing.Point(12, 238);
+            this.playersPanel.Controls.Add(this.player2tB);
+            this.playersPanel.Controls.Add(this.player1tB);
+            this.playersPanel.Location = new System.Drawing.Point(0, 237);
             this.playersPanel.Name = "playersPanel";
             this.playersPanel.Size = new System.Drawing.Size(200, 123);
             this.playersPanel.TabIndex = 2;
@@ -121,6 +150,7 @@
             this.SaveBtn.TabIndex = 6;
             this.SaveBtn.Text = "Mentés";
             this.SaveBtn.UseVisualStyleBackColor = true;
+            this.SaveBtn.Click += new System.EventHandler(this.SaveBtn_Click);
             // 
             // label2
             // 
@@ -142,46 +172,67 @@
             this.label1.TabIndex = 3;
             this.label1.Text = "Játékos 1";
             // 
-            // textBox2
+            // player2tB
             // 
-            this.textBox2.Location = new System.Drawing.Point(88, 49);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 4;
+            this.player2tB.Location = new System.Drawing.Point(88, 49);
+            this.player2tB.Name = "player2tB";
+            this.player2tB.Size = new System.Drawing.Size(100, 20);
+            this.player2tB.TabIndex = 4;
             // 
-            // textBox1
+            // player1tB
             // 
-            this.textBox1.Location = new System.Drawing.Point(88, 14);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 3;
+            this.player1tB.Location = new System.Drawing.Point(88, 14);
+            this.player1tB.Name = "player1tB";
+            this.player1tB.Size = new System.Drawing.Size(100, 20);
+            this.player1tB.TabIndex = 3;
             // 
-            // kékToolStripMenuItem
+            // movementscB
             // 
-            this.kékToolStripMenuItem.Name = "kékToolStripMenuItem";
-            this.kékToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.kékToolStripMenuItem.Text = "Kék";
-            this.kékToolStripMenuItem.Click += new System.EventHandler(this.ThemeClick);
+            this.movementscB.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.movementscB.FormattingEnabled = true;
+            this.movementscB.Items.AddRange(new object[] {
+            "Bal",
+            "Jobb",
+            "Fel",
+            "Le"});
+            this.movementscB.Location = new System.Drawing.Point(17, 62);
+            this.movementscB.Name = "movementscB";
+            this.movementscB.Size = new System.Drawing.Size(217, 21);
+            this.movementscB.TabIndex = 7;
+            this.movementscB.Visible = false;
             // 
-            // sárgaToolStripMenuItem
+            // Player2Pb
             // 
-            this.sárgaToolStripMenuItem.Name = "sárgaToolStripMenuItem";
-            this.sárgaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.sárgaToolStripMenuItem.Text = "Sárga";
-            this.sárgaToolStripMenuItem.Click += new System.EventHandler(this.ThemeClick);
+            this.Player2Pb.Image = ((System.Drawing.Image)(resources.GetObject("Player2Pb.Image")));
+            this.Player2Pb.Location = new System.Drawing.Point(263, 146);
+            this.Player2Pb.Name = "Player2Pb";
+            this.Player2Pb.Size = new System.Drawing.Size(150, 125);
+            this.Player2Pb.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Player2Pb.TabIndex = 8;
+            this.Player2Pb.TabStop = false;
+            this.Player2Pb.Tag = "2";
+            this.Player2Pb.Visible = false;
+            this.Player2Pb.Click += new System.EventHandler(this.MovePb);
             // 
-            // zöldToolStripMenuItem
+            // Player1Pb
             // 
-            this.zöldToolStripMenuItem.Name = "zöldToolStripMenuItem";
-            this.zöldToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.zöldToolStripMenuItem.Text = "Zöld";
-            this.zöldToolStripMenuItem.Click += new System.EventHandler(this.ThemeClick);
+            this.Player1Pb.Image = ((System.Drawing.Image)(resources.GetObject("Player1Pb.Image")));
+            this.Player1Pb.Location = new System.Drawing.Point(263, 31);
+            this.Player1Pb.Name = "Player1Pb";
+            this.Player1Pb.Size = new System.Drawing.Size(150, 125);
+            this.Player1Pb.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Player1Pb.TabIndex = 6;
+            this.Player1Pb.TabStop = false;
+            this.Player1Pb.Tag = "1";
+            this.Player1Pb.Visible = false;
+            this.Player1Pb.Click += new System.EventHandler(this.MovePb);
             // 
             // mapPb
             // 
-            this.mapPb.Location = new System.Drawing.Point(218, 40);
+            this.mapPb.Image = global::MiniGame.Properties.Resources.kék;
+            this.mapPb.Location = new System.Drawing.Point(263, 31);
             this.mapPb.Name = "mapPb";
-            this.mapPb.Size = new System.Drawing.Size(582, 370);
+            this.mapPb.Size = new System.Drawing.Size(600, 500);
             this.mapPb.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.mapPb.TabIndex = 3;
             this.mapPb.TabStop = false;
@@ -190,20 +241,25 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(910, 589);
+            this.Controls.Add(this.Player2Pb);
+            this.Controls.Add(this.movementscB);
+            this.Controls.Add(this.Player1Pb);
             this.Controls.Add(this.mapPb);
             this.Controls.Add(this.playersPanel);
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Kincskereső 2025";
             this.statusStrip1.ResumeLayout(false);
             this.statusStrip1.PerformLayout();
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.playersPanel.ResumeLayout(false);
             this.playersPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Player2Pb)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Player1Pb)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.mapPb)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -215,19 +271,22 @@
         private System.Windows.Forms.StatusStrip statusStrip1;
         private System.Windows.Forms.ToolStripStatusLabel statusBar;
         private System.Windows.Forms.MenuStrip menuStrip1;
-        private System.Windows.Forms.ToolStripMenuItem témaVálasztóToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem játákosokToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem themeSelectorTSM;
+        private System.Windows.Forms.ToolStripMenuItem playersMenuStrip;
         private System.Windows.Forms.ToolStripMenuItem névjegyToolStripMenuItem;
         private System.Windows.Forms.Panel playersPanel;
         private System.Windows.Forms.Button SaveBtn;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox player2tB;
+        private System.Windows.Forms.TextBox player1tB;
         private System.Windows.Forms.ToolStripMenuItem kékToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem sárgaToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem zöldToolStripMenuItem;
         private System.Windows.Forms.PictureBox mapPb;
+        private System.Windows.Forms.PictureBox Player1Pb;
+        private System.Windows.Forms.ComboBox movementscB;
+        private System.Windows.Forms.PictureBox Player2Pb;
     }
 }
 
--- MiniGame/Form1.cs
+++ MiniGame/Form1.cs
@@ -13,9 +13,12 @@ namespace MiniGame
 {
     public partial class Form1 : Form
     {
+        string player1 = string.Empty;
+        string player2 = string.Empty;
         public Form1()
         {
             InitializeComponent();
+            statusBar.Text = "Válassza ki a témat.";
         }
 
         private void GetPlayers(object sender, EventArgs e)
@@ -47,6 +50,64 @@ namespace MiniGame
             ToolStripMenuItem theme = sender as ToolStripMenuItem;
 
             mapPb.Image = Image.FromFile($@"{Directory.GetCurrentDirectory()}\images\{theme.Text.ToLower()}.png");
+
+            themeSelectorTSM.Enabled = false;
+
+            statusBar.Text = "Adja meg a neveket.";
+
+            playersMenuStrip.Enabled = true;
+        }
+
+        private void ShowMakers(object sender, EventArgs e)
+        {
+            MessageBox.Show("Készítette:\nÁbrahám-Fúrús Csaba\nPaplukács Levente\n2025-26", "Névjegy", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void SaveBtn_Click(object sender, EventArgs e)
+        {
+            if(player1tB.Text == string.Empty || player2tB.Text == string.Empty)
+            {
+                statusBar.Text = "Töltse ki mind a kettő név mezőt.";
+            }
+            else if(player1tB.Text.ToLower() == player2tB.Text.ToLower())
+            {
+                statusBar.Text = "A két név nem lehet ugyan az.";
+            }
+            else
+            {
+                player1 = player1tB.Text;
+                player2 = player2tB.Text;
+
+                playersMenuStrip.Enabled = false;
+
+                statusBar.Text = $"A játék elkezdőtött. {player1} lép";
+
+                playersPanel.Visible = false;
+
+                movementscB.Visible = true;
+            }
+
+        }
+        private void MovePb(object sender, EventArgs e)
+        {
+            PictureBox CurrentPicture = sender as PictureBox;
+
+            switch(movementscB.SelectedItem.ToString())
+            {
+                case "Fel":
+                    CurrentPicture.Location = new Point(CurrentPicture.Location.X, CurrentPicture.Location.Y + 150);
+                    break;
+                case "Le":
+                    CurrentPicture.Location = new Point(CurrentPicture.Location.X, CurrentPicture.Location.Y - 150);
+                    break;
+                case "Bal":
+                    CurrentPicture.Location = new Point(CurrentPicture.Location.X - 125, CurrentPicture.Location.Y);
+                    break;
+                case "Jobb":
+                    CurrentPicture.Location = new Point(CurrentPicture.Location.X + 125, CurrentPicture.Location.Y);
+                    break;
+            }
+                
         }
     }
 }
